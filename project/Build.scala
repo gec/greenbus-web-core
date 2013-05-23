661525920d8538fbc2ab690d058b4f7275f5ad7b
Flint O'Brien <fobrien@greenenergycorp.com>
2013-05-23 18:10:47 -0400

    Udated project Build.scala. Mainly cleanup.

--- project/Build.scala
+++ project/Build.scala
@@ -4,27 +4,20 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "coral"
-  val appVersion      = "0.1.0"
-  val playVersion     = "2.1.1"
-  val totalGridRelease = "https://repo.totalgrid.org/artifactory/totalgrid-release"
-  val totalGridSnapshot    = "https://repo.totalgrid.org/artifactory/totalgrid-private-snapshot"
-  val totalGridVersion = "0.5.0-SNAPSHOT" // "0.4.8"
-
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    "com.weiglewilczek.slf4s" % "slf4s_2.9.1" % "1.0.7",
-    "org.totalgrid.reef" % "reef-client" % totalGridVersion,
-    "org.totalgrid.reef" % "reef-service-client" % totalGridVersion
-  )
+  val appName           = "coral"
+  val appVersion        = "0.1.0-SNAPSHOT"
+  val playVersion       = "2.1.1"
+  val totalGridRelease  = "https://repo.totalgrid.org/artifactory/totalgrid-release"
+  val totalGridSnapshot = "https://repo.totalgrid.org/artifactory/totalgrid-private-snapshot"
+  val reefVersion       = "0.5.0-SNAPSHOT" // "0.4.8"
 
   lazy val baseSettings = Seq(
     version            := appVersion,
-  // Need these scala versions or it tries Scala-2.9.2
+    // Need these scala versions or it tries Scala-2.9.2
     scalaVersion       := "2.10.0",
-    scalaBinaryVersion := "2.10",
-    crossScalaVersions := Seq("2.10.0"),
-    organization       := "org.totalgrid",
+    //scalaBinaryVersion := "2.10",
+    //crossScalaVersions := Seq("2.10.0"),
+    organization       := "org.totalgrid.coral",
     scalacOptions += "-feature", // show compiler warnings for language features
     scalacOptions += "-unchecked", // compiler warnings for type aliases and ???
     resolvers += "scala-tools" at "http://repo.typesafe.com/typesafe/scala-tools-releases-cache",
@@ -34,23 +27,14 @@ object ApplicationBuild extends Build {
     //routesImport += "models.QueryBinders._"
   )
 
-  /*
-  val main = play.Project(appName, appVersion, appDependencies)
-    .settings(baseSettings: _*)
-    .settings(
-      libraryDependencies += "play" %% "play-test" % "2.1.1",
-      name := appName
-    )
-  */
-
   lazy val core = Project("core", base = file("core"))
     .settings(baseSettings: _*)
     .settings(
+      name := appName,
       libraryDependencies += "play" %% "play" % playVersion,
-      libraryDependencies += "org.totalgrid.reef" % "reef-client" % totalGridVersion,
-      libraryDependencies += "org.totalgrid.reef" % "reef-service-client" % totalGridVersion,
-      libraryDependencies += "org.mockito" % "mockito-all" % "1.9.5",
-      name := appName
+      libraryDependencies += "org.totalgrid.reef" % "reef-client" % reefVersion,
+      libraryDependencies += "org.totalgrid.reef" % "reef-service-client" % reefVersion,
+      libraryDependencies += "org.mockito" % "mockito-all" % "1.9.5"
     )
 
   lazy val test = Project("test", base = file("test"))
@@ -64,10 +48,18 @@ object ApplicationBuild extends Build {
 
   lazy val sample = play.Project("sample", path = file("sample"))
     .settings(baseSettings: _*)
+    .settings(
+      publishLocal := {},
+      publish := {}
+    )
     .dependsOn(core, test % "test")
 
   lazy val root = Project("root", base = file("."))
     .settings(baseSettings: _*)
-    .aggregate(core, sample)
+    .settings(
+      publishLocal := {},
+      publish := {}
+    )
+    .aggregate(core, test, sample)
 
 }
