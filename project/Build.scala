04280d9d30aeac0f7ce37cd232f22fb42e9a275d
Flint O'Brien <fobrien@greenenergycorp.com>
2013-05-31 16:21:33 -0400

    Removed top level app. Updated Build.scala to publish to artifactory.

--- project/Build.scala
+++ project/Build.scala
@@ -5,14 +5,13 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName           = "coral"
-  val appVersion        = "0.1.0-SNAPSHOT"
   val playVersion       = "2.1.1"
   val totalGridRelease  = "https://repo.totalgrid.org/artifactory/totalgrid-release"
   val totalGridSnapshot = "https://repo.totalgrid.org/artifactory/totalgrid-private-snapshot"
   val reefVersion       = "0.5.0-SNAPSHOT" // "0.4.8"
 
   lazy val baseSettings = Seq(
-    version            := appVersion,
+    version            := "0.1.0-SNAPSHOT",
     // Need these scala versions or it tries Scala-2.9.2
     scalaVersion       := "2.10.0",
     //scalaBinaryVersion := "2.10",
@@ -27,6 +26,31 @@ object ApplicationBuild extends Build {
     //routesImport += "models.QueryBinders._"
   )
 
+  lazy val appPublishMavenStyle = true
+  lazy val appPublishArtifactInTest = false
+  lazy val appPomIncludeRepository = { _: MavenRepository => false }
+  lazy val appPublishTo = { (v: String) =>
+    val nexus = "https://repo.totalgrid.org/artifactory/"
+    if (v.trim.endsWith("SNAPSHOT"))
+      Some("snapshots" at nexus + "totalgrid-private-snapshot")
+    else
+      Some("releases"  at nexus + "totalgrid-private-release")
+  }
+  lazy val appPomExtra = {
+    <url>https://github.com/gec/coral.git</url>
+      <licenses>
+        <license>
+          <name>Apache License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:gec/coral.git</url>
+        <connection>scm:git:git@github.com:gec/coral.git</connection>
+      </scm>
+  }
+
   lazy val core = Project("core", base = file("core"))
     .settings(baseSettings: _*)
     .settings(
@@ -34,7 +58,12 @@ object ApplicationBuild extends Build {
       libraryDependencies += "play" %% "play" % playVersion,
       libraryDependencies += "org.totalgrid.reef" % "reef-client" % reefVersion,
       libraryDependencies += "org.totalgrid.reef" % "reef-service-client" % reefVersion,
-      libraryDependencies += "org.mockito" % "mockito-all" % "1.9.5"
+      libraryDependencies += "org.mockito" % "mockito-all" % "1.9.5",
+      publishMavenStyle       := appPublishMavenStyle,
+      publishArtifact in Test := appPublishArtifactInTest,
+      pomIncludeRepository    := appPomIncludeRepository,
+      publishTo               <<=(version)(appPublishTo),
+      pomExtra                := appPomExtra
     )
 
   lazy val test = Project("test", base = file("test"))
@@ -42,7 +71,12 @@ object ApplicationBuild extends Build {
     .settings(
       name := appName + ".test",
       libraryDependencies += "play" %% "play-test" % playVersion,
-      libraryDependencies += "org.mockito" % "mockito-all" % "1.9.5"
+      libraryDependencies += "org.mockito" % "mockito-all" % "1.9.5",
+      publishMavenStyle       := appPublishMavenStyle,
+      publishArtifact in Test := appPublishArtifactInTest,
+      pomIncludeRepository    := appPomIncludeRepository,
+      publishTo               <<=(version)(appPublishTo),
+      pomExtra                := appPomExtra
     )
     .dependsOn(core)
 
