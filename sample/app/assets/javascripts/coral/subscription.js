312e38c5f25060f520060f40f4835aafae97c7a3
Flint O'Brien <fobrien@greenenergycorp.com>
2014-09-23 10:14:30 -0400

    Subscription ID converted to a UUID.

--- sample/app/assets/javascripts/coral/subscription.js
+++ sample/app/assets/javascripts/coral/subscription.js
@@ -52,7 +52,6 @@ define([
         var webSocketPendingTasks = []
 
         var subscription = {
-            idCounter: 0,
             listeners: {}   // { subscriptionId: { message: listener, error: listener}, ...}
         };
 
@@ -188,11 +187,20 @@ define([
             }
         }
 
+        function generateUUID(){
+          var d = new Date().getTime();
+          var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+          });
+          return uuid;
+        }
+
         function makeSubscriptionId( json) {
             var messageKey = Object.keys( json)[0]
-            subscription.idCounter ++;
             // add the messageKey just for easier debugging.
-            return "subscription." + messageKey + "." + subscription.idCounter;
+            return "subscription." + messageKey + "." + generateUUID();
         }
 
         function addSubscriptionIdToMessage( json) {
