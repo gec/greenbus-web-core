944fd54b6df116c07942c3bcd0a3e388011d8ee4
Flint O'Brien <fobrien@greenenergycorp.com>
2015-03-26 11:34:05 -0400

    Updated NavigationElement to use latest Json parsing. Can read/write NavigationElements.

--- core/src/main/scala/io/greenbus/web/config/Navigation.scala
+++ core/src/main/scala/io/greenbus/web/config/Navigation.scala
@@ -7,6 +7,7 @@ import play.api.libs.json._
  * @author Flint O'Brien
  */
 object Navigation {
+  import io.greenbus.web.util.EnumUtils
 
   /**
    * For NavigationItemSource, where will the results be inserted?
@@ -17,6 +18,7 @@ object Navigation {
     val CHILDREN = Value  // Result items will be inserted as children of NavigationItem.
   }
   import InsertLocation._
+  implicit val insertLocationFormat = EnumUtils.enumFormat(InsertLocation)
 
   sealed trait ItemLoadable {
     def sourceUrl: String
@@ -24,62 +26,46 @@ object Navigation {
   }
 
   sealed trait NavigationElement
-  sealed trait NavigationDivider extends NavigationElement
-  case object NavigationDivider extends NavigationDivider
+  case object NavigationDivider extends NavigationElement
   case class NavigationHeader( label: String) extends NavigationElement
   case class NavigationItem( label: String, id: String, route: String, selected: Boolean = false, children: List[NavigationElement] = List()) extends NavigationElement
   case class NavigationItemSource( label: String, id: String, route: String, val sourceUrl: String, val insertLocation: InsertLocation, selected: Boolean = false, val children: List[NavigationElement] = List()) extends NavigationElement with ItemLoadable
 
 
-  implicit val navigationHeaderWrites = new Writes[NavigationHeader] {
-    def writes( o: NavigationHeader): JsValue =
-      Json.obj(
-        "type" -> "header",
-        "label" -> o.label
-      )
-  }
-  implicit val navigationDividerWrites = new Writes[NavigationDivider] {
-    def writes( o: NavigationDivider): JsValue =
-      Json.obj(
-        "type" -> "divider"
-      )
-  }
-  implicit val navigationItemWrites = new Writes[NavigationItem] {
-    def writes( o: NavigationItem): JsValue =
-      Json.obj(
-        "type" -> "item",
-        "label" -> o.label,
-        "id" -> o.id,
-        "route" -> o.route,
-        "selected" -> o.selected,
-        "children" -> o.children
-      )
-  }
-  implicit val navigationItemSourceWrites = new Writes[NavigationItemSource] {
-    def writes( o: NavigationItemSource): JsValue =
-      Json.obj(
-        "type" -> "item",
-        "label" -> o.label,
-        "id" -> o.id,
-        "route" -> o.route,
-        "sourceUrl" -> o.sourceUrl,
-        "insertLocation" -> o.insertLocation.toString,
-        "selected" -> o.selected,
-        "children" -> o.children
-      )
-  }
-  implicit val navigationElementWrites = new Writes[NavigationElement] {
-    def writes( o: NavigationElement): JsValue =
-      o match {
-        case item: NavigationDivider => navigationDividerWrites.writes( item)
-        case item: NavigationHeader => navigationHeaderWrites.writes( item)
-        case item: NavigationItem => navigationItemWrites.writes( item)
-        case item: NavigationItemSource => navigationItemSourceWrites.writes( item)
+  object NavigationElement {
+    def unapply(navigationElement: NavigationElement): Option[(String, JsValue)] = {
+      val (prod: Product, sub) = navigationElement match {
+        // case object NavigationDivider is a value, not a type.
+        case NavigationDivider => (NavigationDivider, Json.toJson(Json.obj()))
+        case b: NavigationHeader => (b, Json.toJson(b)(NavigationHeader.navigationHeaderFormat))
+        case b: NavigationItem => (b, Json.toJson(b)(NavigationItem.navigationItemFormat))
+        case b: NavigationItemSource => (b, Json.toJson(b)(NavigationItemSource.navigationItemSourceFormat))
       }
+      Some(prod.productPrefix -> sub)
+    }
+
+    def apply(`class`: String, data: JsValue): NavigationElement = {
+      (`class` match {
+        case "NavigationDivider" => JsSuccess[NavigationElement](NavigationDivider)
+        case "NavigationHeader" => Json.fromJson[NavigationHeader](data)(NavigationHeader.navigationHeaderFormat)
+        case "NavigationItem" => Json.fromJson[NavigationItem](data)(NavigationItem.navigationItemFormat)
+        case "NavigationItemSource" => Json.fromJson[NavigationItemSource](data)(NavigationItemSource.navigationItemSourceFormat)
+      }).get
+    }
+    implicit val navigationElementFormat = Json.format[NavigationElement]
+  }
+  object NavigationHeader {
+    implicit val navigationHeaderFormat = Json.format[NavigationHeader]
+  }
+  object NavigationItem {
+    implicit val navigationItemFormat = Json.format[NavigationItem]
+  }
+  object NavigationItemSource {
+    implicit val navigationItemSourceFormat = Json.format[NavigationItemSource]
   }
 
   implicit val navigationElementListWrites: Writes[List[NavigationElement]] = new Writes[List[NavigationElement]] {
-    def writes( o: List[NavigationElement]): JsValue = JsArray( o.map( navigationElementWrites.writes))
+    def writes( o: List[NavigationElement]): JsValue = JsArray( o.map( NavigationElement.navigationElementFormat.writes))
   }
 
 
